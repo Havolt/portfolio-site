775c0b3df8477c7110d2a06624e31e2a04549a33
Mark Fitzpatrick <markfitz815@gmail.com>
2018-09-11 21:38:13 +0100

    add animated intro text to infoSection

--- public/scripts/indexMain.js
+++ public/scripts/indexMain.js
@@ -4,6 +4,10 @@ if(typeof Vue === 'undefined') {
     document.body.appendChild(newVue);
 }
 
+
+
+onload = () => {
+
 //Toggles if element has the displayNone class
 function toggleDisplay(el) {
     el.classList.toggle('displayNone');
@@ -11,13 +15,38 @@ function toggleDisplay(el) {
 //Initial functions when app is loaded
 function initFunctions() {
     toggleDisplay(document.querySelector('#vApp'));
+    setTimeout(() => {vueApp.intTxtAnimate(vueApp.intTxtAnimData)}, 10);
 }
 
-onload = () =>{
     const vueApp = new Vue({
         el: '#vApp',
         data: {
-            test: 'test data here'
+            test: 'test data here',
+            intTxtAnimData: {
+                text: 'Hi. I\'m Mark. A Front End Web Developer'.split(''),
+                display: []
+
+            },
+            headerList: [
+                'Home',
+                'About',
+                'Portfolio',
+                'Contact'
+            ]
+        },
+        methods: {
+            intTxtAnimate: function(v) {
+                v.display.push(v.text[v.display.length]);
+
+                if(v.display.length !== v.text.length) {
+                    setTimeout(() => {vueApp.intTxtAnimate(vueApp.intTxtAnimData)}, 40)
+                }
+            }
+        },
+        computed: {
+            intTxtInfo: function() {
+                return this.intTxtAnimData.display.join('');
+            }
         }
     })
 
