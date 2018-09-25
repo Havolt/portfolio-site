a93e99119d0307022072f2b65e49068251763495
Mark Fitzpatrick <markfitz815@gmail.com>
2018-09-25 13:37:22 +0100

    change vueLoad to work with portfolio pages. Add in files for portfolio header

---
+++ public/scripts/port/portHeader.js
@@ -0,0 +1,7 @@
+
+const vh = new Vue({
+    el: '#vHead',
+    data: {
+        test: 'moo'
+    }
+})
--- public/scripts/utility/vueLoad.js
+++ public/scripts/utility/vueLoad.js
@@ -1,3 +1,5 @@
+
+
 if(typeof Vue === 'undefined') {
     const newVue = document.createElement('script');
     newVue.src="/scripts/utility/vue.js";
@@ -5,5 +7,17 @@ if(typeof Vue === 'undefined') {
 }
 
 onload = () => {
-    scriptLoad('indexMain.js')
+    let checkPort = window.location.pathname.split('').splice(0, 6).join('');
+    if(window.location.pathname == '/'){
+        scriptLoad('indexMain.js')
+    } else if(checkPort == '/port/') {
+        const styleS = document.createElement("link");
+        styleS.type = "text/css";
+        styleS.rel = "stylesheet";
+        styleS.href = "/styles/port/portHeader.css";
+        document.head.appendChild(styleS);
+
+        let pathName = window.location.pathname.split('').splice(6).join('');
+        scriptLoad(['/port/portHeader.js'])
+    }
 }
