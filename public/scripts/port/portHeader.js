56004c254decf309249948e932ffabc2832e1117
Mark Fitzpatrick <markfitz815@gmail.com>
2018-09-25 17:49:52 +0100

    fix sizing of flex boxes in header and add info book icon with toggle

--- public/scripts/port/portHeader.js
+++ public/scripts/port/portHeader.js
@@ -2,7 +2,16 @@
 const vh = new Vue({
     el: '#vHead',
     data: {
+        //Logo Section
         logoName: 'Mark F',
-        logoNameFull: 'Mark Fitzpatrick'
+        logoNameFull: 'Mark Fitzpatrick',
+        
+        //Info Section
+        infoOpen: false
+    },
+    methods: {
+        toggleInfo: function() {
+            this.infoOpen = !this.infoOpen;
+        }
     }
 })
