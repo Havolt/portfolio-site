ec11f57293852176d5fd37c9b4b4db0944dba278
Mark Fitzpatrick <markfitz815@gmail.com>
2018-10-10 13:42:08 +0100

    add nodemailer to application

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 const bodyParser = require('body-parser');
 const fs = require('fs');
+const nodemailer = require('nodemailer');
 const portfolioLinks = ['to-do', 'calculator', 'checkers', 'nc-creepy', 'offline-hacker', 'prev-portfolio', 'tetris', 'wiki-search'];
 
 app.use(express.static('public'))
@@ -13,8 +14,40 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/views/index.html');
 });
 
+var transporter = nodemailer.createTransport({
+    host: "smtp-mail.outlook.com", 
+    secureConnection: false, 
+    port: 587, 
+    tls: {
+       ciphers:'SSLv3'
+    },
+    auth: {
+        user: 'markfitz101@hotmail.com', 
+        pass: ''
+    }
+});
+
+
+        
+
 app.post('/form', (req, res) => {
     let parseObj = JSON.stringify(req.body.name) + '\n' + JSON.stringify(req.body.email) + '\n' + JSON.stringify(req.body.comment) + '\n \n \n';
+    
+    const mailOpt = {
+        from: 'Portfolio ✔ <markfitz101@hotmail.com>',
+        to: 'markfitz815@gmail.com',
+        subject: "Portfolio Message",
+        text: 'Hello ' + parseObj,
+        html: "<p>Hello " + parseObj + " </p>",
+        bcc: "markfitz815@gmail.com"
+    };
+    transporter.sendMail(mailOpt, function(error, inf){
+        if(error){
+            console.log(error);
+        }else{
+            console.log('Message sent: ' + inf.response);
+        }
+    });    
     fs.appendFile('comments.txt', parseObj, function(err) {
         if(err) throw err;
     })
